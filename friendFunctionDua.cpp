0abbb6883f6763a74f99c56095d404b89f676faf
vannn-tech <jovan20166@gmail.com>
2026-04-27 18:07:06 +0700

    tambah class pelajar + konstruktor default

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -9,3 +9,13 @@ public:
     void showNilaiPelajar(pelajar &x);
 };
 
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &x);
+};
+

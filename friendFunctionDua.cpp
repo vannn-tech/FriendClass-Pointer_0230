d19bae48cee5350cfc08bca98e4ff24a2a82e1c0
vannn-tech <jovan20166@gmail.com>
2026-04-27 18:06:47 +0700

    inisialisasi class manusia + forward declaration 2

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -9,25 +9,3 @@ public:
     void showNilaiPelajar(pelajar &x);
 };
 
-class pelajar
-{
-private:
-    int nilai;
-
-public:
-    pelajar() { nilai = 100; }
-    friend void manusia::showNilaiPelajar(pelajar &x);
-};
-
-void manusia::showNilaiPelajar(pelajar &x)
-{
-    cout << x.nilai;
-}
-
-int main()
-{
-    manusia budi;
-    pelajar pbudi;
-    budi.showNilaiPelajar(pbudi);
-    return 0;
-}
